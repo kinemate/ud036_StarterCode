258534cd2ddfe92fa647b3d349bc7e90b3c12bb5
kinemate <jonas.veya@gmail.com>
2016-11-04 11:09:05 +0100

    With extra film

--- entertainment_center.py
+++ entertainment_center.py
@@ -6,5 +6,15 @@ toy_story = media.Movie("Toy Story",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
 
-movies = [toy_story]
+zootopia = media.Movie("Zootopia",
+                        "Buddy cop comedy adventure",
+                        "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
+                        "https://www.youtube.com/watch?v=jWM0ct-OLsM")
+
+captain_america_civilwar = media.Movie("Captain America: Civil war,
+                        "Superhero movie",
+                        "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")
+
+movies = [toy_story, zootopia, captain_america_civilwar]
 fresh_tomatoes.open_movies_page(movies)
