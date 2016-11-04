3d0829076ee6a624a89ca63ff0a552d3e60ebcf6
kinemate <jonas.veya@gmail.com>
2016-11-04 11:27:49 +0100

    ysync

--- entertainment_center.py
+++ entertainment_center.py
@@ -11,7 +11,7 @@ zootopia = media.Movie("Zootopia",
                         "https://upload.wikimedia.org/wikipedia/en/e/ea/Zootopia.jpg",
                         "https://www.youtube.com/watch?v=jWM0ct-OLsM")
 
-captain_america_civilwar = media.Movie("Captain America: Civil war,
+captain_america_civilwar = media.Movie("Captain America: Civil war",
                         "Superhero movie",
                         "https://upload.wikimedia.org/wikipedia/en/5/53/Captain_America_Civil_War_poster.jpg",
                         "https://www.youtube.com/watch?v=vwyZH85NQC4")
